e344ddfe67a7160cff1cb8183880494d3761c4f1
Alejandro Ismael Silva <alex.sai95@gmail.com>
2017-03-22 15:45:02 -0300

    Removed glide-transformations lib

--- app/src/main/java/com/uncmorfi/balance/UserCursorAdapter.java
+++ app/src/main/java/com/uncmorfi/balance/UserCursorAdapter.java
@@ -2,6 +2,9 @@ package com.uncmorfi.balance;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +15,13 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.uncmorfi.R;
 import com.uncmorfi.balance.model.User;
 import com.uncmorfi.balance.model.UsersContract;
 
 import java.io.Serializable;
 import java.util.Locale;
-
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 
 public class UserCursorAdapter extends RecyclerView.Adapter<UserCursorAdapter.UserViewHolder> {
@@ -84,12 +86,21 @@ public class UserCursorAdapter extends RecyclerView.Adapter<UserCursorAdapter.Us
         holder.cardText.setText(user.getCard());
         holder.typeText.setText(user.getType());
         holder.balanceText.setText(String.format(Locale.US, "$ %d", user.getBalance()));
+
         Glide.with(mContext)
                 .load(USER_IMAGES_URL + user.getImage())
+                .asBitmap()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .centerCrop()
-                .bitmapTransform(new CropCircleTransformation(mContext))
-                .into(holder.userImage);
+                .into(new BitmapImageViewTarget(holder.userImage) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(mContext.getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                holder.userImage.setImageDrawable(circularBitmapDrawable);
+            }
+        });
     }
 
     @Override

60b7457bcbf6cb5fc05802988831d4d0a15d8480
Alejandro Ismael Silva <alex.sai95@gmail.com>
2017-03-31 23:39:26 -0300

    Set version name in about from Gradle config

--- app/src/main/java/com/uncmorfi/about/AboutDialog.java
+++ app/src/main/java/com/uncmorfi/about/AboutDialog.java
@@ -1,11 +1,14 @@
 package com.uncmorfi.about;
 
 import android.app.Dialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.TextView;
 
 import com.uncmorfi.R;
 
@@ -26,9 +29,24 @@ public class AboutDialog extends DialogFragment {
 
         View v = View.inflate(getContext(), R.layout.dialog_about, null);
 
-        builder.setView(v);
+        TextView versionNameView = (TextView) v.findViewById(R.id.version_name);
+        setVersionName(versionNameView);
 
+        builder.setView(v);
         return builder.create();
     }
 
+    private void setVersionName(TextView v) {
+        String versionName = "";
+
+        try {
+            PackageInfo packageInfo = getContext().getPackageManager()
+                    .getPackageInfo(getContext().getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        v.setText(versionName);
+    }
 }

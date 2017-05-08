1dec9c1bd6ceff66d6624bc39bb47bd251e0ee36
Alejandro Ismael Silva <alex.sai95@gmail.com>
2017-05-08 12:32:45 -0300

    Renamed card_item_user to item_user and small changes on this

--- app/src/main/java/com/uncmorfi/balance/UserCursorAdapter.java
+++ app/src/main/java/com/uncmorfi/balance/UserCursorAdapter.java
@@ -73,7 +73,7 @@ class UserCursorAdapter extends RecyclerView.Adapter<UserCursorAdapter.UserViewH
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.card_item_user, parent, false);
+                .inflate(R.layout.item_user, parent, false);
         return new UserViewHolder(v);
     }
 

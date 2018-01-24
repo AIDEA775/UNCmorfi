8174e9de942741ab11938b5c5f368fe98fa8c902
Alejandro Ismael Silva <alex.sai95@gmail.com>
2018-01-24 02:17:02 -0300

    New online help, removed the old. Close #19

--- app/src/main/java/com/uncmorfi/help/HelpFragment.java
+++ app/src/main/java/com/uncmorfi/help/HelpFragment.java
@@ -2,22 +2,20 @@ package com.uncmorfi.help;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.uncmorfi.R;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Ayuda o FAQ.
- * Usa a {@link HelpExpandableRecyclerAdapter} para llenar un {@link RecyclerView}
+ * Muestra una página web alojada en el mismo repositorio de github.
  */
 public class HelpFragment extends Fragment {
+    private static final String URL = "https://aidea775.github.io/UNCmorfi/resources/help/";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,21 +26,11 @@ public class HelpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_help, container, false);
-        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.help_list);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new HelpExpandableRecyclerAdapter(getContext(), getQuestionList()));
+        WebView myWebView = v.findViewById(R.id.help_content);
+        myWebView.loadUrl(URL);
+
         return v;
-    }
-
-    private List<Question> getQuestionList() {
-        String[] questions = getResources().getStringArray(R.array.help_string_array);
-
-        List<Question> questionList = new ArrayList<>();
-        for (int i = 0; i < questions.length; i += 2) {
-            questionList.add(new Question(questions[i], questions[i+1]));
-        }
-        return questionList;
     }
 
     @Override

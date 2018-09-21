0d54e8789a22dcefa8cae0d6e323df5af87c9f77
Mauricio N. Guignard <mauguignard@gmail.com>
2018-09-21 16:31:26 -0300

    Converted RefreshMenuTask to Kotlin

---
+++ app/src/main/java/com/uncmorfi/menu/RefreshMenuTask.kt
@@ -0,0 +1,58 @@
+package com.uncmorfi.menu
+
+import android.content.Context
+import android.os.AsyncTask
+
+import com.uncmorfi.helpers.ConnectionHelper
+import com.uncmorfi.helpers.MemoryHelper
+
+import java.io.IOException
+import java.lang.ref.WeakReference
+
+import com.uncmorfi.menu.MenuFragment.MENU_FILE
+
+/**
+ * Descarga y parsea el menú de la semana.
+ */
+internal class RefreshMenuTask(context: Context, private val mListener: RefreshMenuListener) :
+        AsyncTask<Void, Void, List<DayMenu>>() {
+    private val mContext: WeakReference<Context> = WeakReference(context)
+
+    internal interface RefreshMenuListener {
+        fun onRefreshMenuSuccess(menu: List<DayMenu>)
+        fun onRefreshMenuFail()
+    }
+
+    override fun doInBackground(vararg params: Void): List<DayMenu>? {
+        val download: String
+        try {
+            download = ConnectionHelper.downloadFromUrlByGet(URL)
+
+            val context = mContext.get()
+            if (context != null && needSaveMenu(context, download)) {
+                MemoryHelper.saveToStorage(context, MENU_FILE, download)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+
+        return DayMenu.fromJson(download)
+    }
+
+    override fun onPostExecute(result: List<DayMenu>?) {
+        if (result == null)
+            mListener.onRefreshMenuFail()
+        else
+            mListener.onRefreshMenuSuccess(result)
+    }
+
+    private fun needSaveMenu(context: Context, menu: String): Boolean {
+        val menuSaved = MemoryHelper.readHeadFromStorage(context, MENU_FILE)
+        return menuSaved == null || !menu.startsWith(menuSaved)
+    }
+
+    companion object {
+        private const val URL = "http://uncmorfi.georgealegre.com/menu"
+    }
+}

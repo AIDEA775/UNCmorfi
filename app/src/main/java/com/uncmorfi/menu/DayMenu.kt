dd588d6a0ab5ed71082809a1750af7c476ee24d2
Mauricio N. Guignard <mauguignard@gmail.com>
2018-09-21 14:53:14 -0300

    Converted DayMenu to Kotlin

---
+++ app/src/main/java/com/uncmorfi/menu/DayMenu.kt
@@ -0,0 +1,55 @@
+package com.uncmorfi.menu
+
+import com.uncmorfi.helpers.ParserHelper
+
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+import java.util.ArrayList
+import java.util.Collections
+import java.util.Date
+
+class DayMenu internal constructor(date: String, foods: JSONArray) {
+    lateinit var date: Date
+    private lateinit var mFood: MutableList<String>
+
+    val food: List<String>
+        get() = mFood
+
+    init {
+        try {
+            this.date = ParserHelper.stringToDate(date)
+            mFood = ArrayList()
+            for (i in 0 until foods.length()) {
+                mFood.add(foods.getString(i))
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun fromJson(source: String): List<DayMenu> {
+            val menuList = ArrayList<DayMenu>()
+
+            try {
+                val result = JSONObject(source)
+                val week = result.getJSONObject("menu")
+
+                val keys = week.keys()
+                while (keys.hasNext()) {
+                    val key = keys.next() as String
+                    menuList.add(DayMenu(key, week.getJSONArray(key)))
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            Collections.sort(menuList, ParserHelper.MenuDayComparator())
+            return menuList
+        }
+    }
+}

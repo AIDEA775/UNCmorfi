ed2c11db85375c5c387033cff473a4da21f5752b
Alejandro Ismael Silva <silva.alejandro.ismael@gmail.com>
2018-10-05 17:34:06 -0300

    Refactoring About

--- app/src/main/java/com/uncmorfi/about/AboutDialog.kt
+++ app/src/main/java/com/uncmorfi/about/AboutDialog.kt
@@ -5,24 +5,19 @@ import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import android.view.View
-import android.widget.TextView
 import com.uncmorfi.BuildConfig
 import com.uncmorfi.R
+import kotlinx.android.synthetic.main.dialog_about.view.*
 
 class AboutDialog : DialogFragment() {
 
-    private val versionName: String
-        get() {
-            return BuildConfig.VERSION_NAME
-        }
+    private val version: String = BuildConfig.VERSION_NAME
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireActivity())
 
         val v = View.inflate(context, R.layout.dialog_about, null)
-
-        val versionText = v.findViewById<TextView>(R.id.version_name)
-        versionText.text = versionName
+        v.versionName.text = version
 
         builder.setView(v)
         return builder.create()
